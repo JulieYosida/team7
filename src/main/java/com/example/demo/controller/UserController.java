bc52f7f7ac55f3d47033b48b81c89b14842d7e13
S0611 <S0611@22S0611>
2024-11-08 10:14:46 +0900

    管理者

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,41 +4,47 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.example.demo.entity.User;
+import com.example.demo.repository.UserRepository;
 import com.example.demo.service.UserService;
 
-@RestController
-@RequestMapping("/api/users")
+@Controller
 public class UserController {
 
     @Autowired
-    private UserService userService;
+    private UserRepository userrepository;
+    
+    @Autowired
+    private UserService userservice;
 
     // ユーザを作成するためのPOSTリクエスト（JSON形式を受け取る）
     @PostMapping
     public ResponseEntity<String> createUser(@RequestBody User user) {
-        userService.createUser(user.getEmail(), user.getPassword(), user.getName());
+        userservice.createUser(user.getEmail(), user.getPassword(), user.getName());
         return ResponseEntity.ok("User created successfully");
     }
 
-    // ユーザ一覧を取得するためのGETリクエスト（/listというパスに変更）
-    @GetMapping("/list")
-    public ResponseEntity<List<User>> getAllUsers() {
-        List<User> users = userService.getAllUsers();
-        return ResponseEntity.ok(users);
-    }
+    
+    @RequestMapping(path = "/administrator", method = RequestMethod.GET)
+	public String showAttendance(Model model) {
+		List<User> user = userrepository.findAll();
+		model.addAttribute("user", user);
+		return "administrator";
+	}
 
     // 特定のユーザをIDで取得するためのGETリクエスト
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable int id) {
-        User user = userService.getUserById(id);
+        User user = userservice.getUserById(id);
         if (user != null) {
             return ResponseEntity.ok(user);
         } else {
